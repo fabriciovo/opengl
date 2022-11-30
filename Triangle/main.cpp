2fc448c39e055869ba3f66b735dde8a51d898ac7
Fabricio Varisco <fabricio.v.o@hotmail.com>
2022-11-29 22:49:02 -0300

    GB

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -8,16 +8,16 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <GLM/gtc/type_ptr.hpp>
 #include "shaderClass.h"
+#include "writter.hpp"
 
 
 using namespace std;
 
-vector<glm::vec3*>* CreateBSpline(vector<glm::vec3*>* points);
+void CreateBSpline(vector<glm::vec3*>* points);
 
 
 vector<float> points;
 vector<float> bSplineCurve;
-vector<glm::vec3*>* controlPointsVec = new vector<glm::vec3*>();
 vector<glm::vec3*>* selectedPoints = new vector<glm::vec3*>();
 
 const unsigned int SCR_WIDTH = 800;
@@ -25,30 +25,21 @@ const unsigned int SCR_HEIGHT = 800;
 
 
 GLuint vaoPoints, vboPoints;
-GLuint vaoPointLines, vboPointLines;
 GLuint vaoSpline, vboSpline;
 
 
 
-vector<glm::vec3*>* CreateBSpline(vector<glm::vec3*>* points) {
+void CreateBSpline(vector<glm::vec3*>* points) {
 
-	bSplineCurve.clear();
-	vector<glm::vec3*>* bspline = new vector<glm::vec3*>();
+	//bSplineCurve.clear();
 	vector<glm::vec3*>* temp = new vector<glm::vec3*>();
-	vector<glm::vec3*>* temp2 = new vector<glm::vec3*>();
 
 	int size = points->size();
 	for (int i = 0; i < size; i++) {
 		temp->push_back(new glm::vec3(points->at(i)->x, points->at(i)->y, points->at(i)->z));
 	}
 
-
-	temp->push_back(points->at(0));
-	temp->push_back(points->at(1));
-	temp->push_back(points->at(2));
-
-
-	for (int i = 0; i < (temp->size() - 3); i++) {
+	/*for (int i = 0; i < (temp->size() - 3); i++) {
 
 		for (int j = 0; j < 100; ++j) {
 
@@ -70,14 +61,46 @@ vector<glm::vec3*>* CreateBSpline(vector<glm::vec3*>* points) {
 				(1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3)->z) / 6);
 
 			glm::vec3* point = new glm::vec3(x, y, z);
-			bspline->push_back(point);
 			bSplineCurve.push_back(x);
 			bSplineCurve.push_back(y);
 			bSplineCurve.push_back(z);
+
+		}
+	}*/
+	//float inc = 1 / 100;
+	int N = temp->size() - 3;
+	//for (int i = 0; i < (temp->size() - 3); i++) {
+
+	//	float inc = static_cast<float>(i) / 99.0;
+	//	for (int t = 0; t <= 1; t += inc) {
+	//		GLfloat x = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->x + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1 % N)->x +  (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2 % N)->x + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3 % N)->x) / 6);
+	//		GLfloat y = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->y + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1 % N)->y + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2 % N)->y + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3 % N)->y) / 6);
+	//		GLfloat z = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->z + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1 % N)->z + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2 % N)->z + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3 % N)->z) / 6);
+	//		bSplineCurve.push_back(x);
+	//		bSplineCurve.push_back(y);
+	//		bSplineCurve.push_back(z);
+	//	}
+	//}
+
+
+	for (int i = 0; i < (temp->size() - 3); i++) {
+
+		for (int j = 0; j < 100; ++j) {
+
+			float t = static_cast<float>(j) / 99.0;
+
+			GLfloat x = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->x + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1)->x + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2)->x + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3)->x) / 6);
+			GLfloat y = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->y + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1)->y + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2)->y + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3)->y) / 6);
+			GLfloat z = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1) * temp->at(i)->z + (3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4) * temp->at(i + 1)->z + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) * temp->at(i + 2)->z + (1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0) * temp->at(i + 3)->z) / 6);
+			bSplineCurve.push_back(x);
+			bSplineCurve.push_back(y);
+			bSplineCurve.push_back(z);
+
 		}
 	}
 
-	return bspline;
+	SplineWriter * w = new SplineWriter(bSplineCurve);
+	w->WriteSpline();
 }
 
 
@@ -86,35 +109,31 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
 	{
 		double xpos, ypos;
+		double realXpos, realYpos;
 		//getting cursor position
 		glfwGetCursorPos(window, &xpos, &ypos);
 		cout << "Cursor Position at (" << xpos  << " : " << ypos << endl;
-		points.push_back((2.0f * xpos / 800 - 1.0f));
-		points.push_back((-2.0f * ypos / 800 + 1.0f));
+		realXpos = 2.0f * xpos / 800 - 1.0f;
+		realYpos = -2.0f * ypos / 800 + 1.0f;
+
+		points.push_back(realXpos);
+		points.push_back(realYpos);
 		points.push_back(0.0f);
 
-		selectedPoints->push_back(new glm::vec3(xpos,ypos,0.0f));
+		selectedPoints->push_back(new glm::vec3(realXpos, realYpos,0.0f));
 
 		glBindVertexArray(vaoPoints);
 		glBindBuffer(GL_ARRAY_BUFFER, vboPoints);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size(), points.data(), GL_DYNAMIC_DRAW);
-
 		
 		if (points.size() > 9) {
 
-			controlPointsVec = CreateBSpline(selectedPoints);
-			//glBindVertexArray(vaoSpline);
-			//glBindBuffer(GL_ARRAY_BUFFER, vboSpline);
-			//glBufferData(GL_ARRAY_BUFFER, sizeof(float) * bSplineCurve.size(), bSplineCurve.data(), GL_STATIC_DRAW);
-			//glEnableVertexAttribArray(0);
-			//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+			CreateBSpline(selectedPoints);
 			glBindVertexArray(vaoSpline);
 			glBindBuffer(GL_ARRAY_BUFFER, vboSpline);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * bSplineCurve.size(), bSplineCurve.data(), GL_DYNAMIC_DRAW);
 
 		}
-
-
 	}
 }
 
@@ -176,21 +195,11 @@ int main()
 
 		glBindVertexArray(vaoPoints);
 		glDrawArrays(GL_POINTS, 0, points.size() / 3);
-		//glDrawArrays(GL_LINE_STRIP, 0, points.size() / 3);
+		glDrawArrays(GL_LINE_STRIP, 0, points.size() / 3);
 
 		glBindVertexArray(vaoSpline);
 		glDrawArrays(GL_LINE_STRIP, 0, bSplineCurve.size() / 3);
 
-
-
-		//glDrawArrays(GL_LINE_STRIP, 0, bSplineCurve.size() / 3);
-		//glBindVertexArray(vaoSpline);
-		////glBindBuffer(GL_ARRAY_BUFFER, vboSpline);
-		////glBufferData(GL_ARRAY_BUFFER, sizeof(float) * bSplineCurve.size(), bSplineCurve.data(), GL_STATIC_DRAW);
-		////glEnableVertexAttribArray(0);
-		////glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-		//glBindVertexArray(vaoSpline);
-		//glDrawArrays(GL_LINE_STRIP, 0, bSplineCurve.size() / 3);
 
 		glfwSwapBuffers(window);
 		
