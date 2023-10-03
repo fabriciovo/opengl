67f1775ff4eb62e9e41e1fdb08419653aff1aab4
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-10-03 20:50:08 -0300

    update

---
+++ LoadObject/Model.cpp
@@ -0,0 +1,538 @@
+#include"Model.h"
+
+Model::Model(const char* file)
+{
+	// Make a JSON object
+	std::string text = get_file_contents(file);
+	JSON = json::parse(text);
+
+	// Get the binary data
+	Model::file = file;
+	data = getData();
+
+	// Traverse all nodes
+	traverseNode(0);
+}
+
+void Model::Draw(Shader& shader, Camera& camera)
+{
+	// Go over all meshes and draw each one
+	for (unsigned int i = 0; i < meshes.size(); i++)
+	{
+		meshes[i].Mesh::Draw(shader, camera, matricesMeshes[i]);
+	}
+}
+
+
+void Model::loadMesh(unsigned int indMesh)
+{
+	// Get all accessor indices
+	unsigned int posAccInd = JSON["meshes"][indMesh]["primitives"][0]["attributes"]["POSITION"];
+	unsigned int normalAccInd = JSON["meshes"][indMesh]["primitives"][0]["attributes"]["NORMAL"];
+	unsigned int texAccInd = JSON["meshes"][indMesh]["primitives"][0]["attributes"]["TEXCOORD_0"];
+	unsigned int indAccInd = JSON["meshes"][indMesh]["primitives"][0]["indices"];
+
+	// Use accessor indices to get all vertices components
+	std::vector<float> posVec = getFloats(JSON["accessors"][posAccInd]);
+	std::vector<glm::vec3> positions = groupFloatsVec3(posVec);
+	std::vector<float> normalVec = getFloats(JSON["accessors"][normalAccInd]);
+	std::vector<glm::vec3> normals = groupFloatsVec3(normalVec);
+	std::vector<float> texVec = getFloats(JSON["accessors"][texAccInd]);
+	std::vector<glm::vec2> texUVs = groupFloatsVec2(texVec);
+
+	// Combine all the vertex components and also get the indices and textures
+	std::vector<Vertex> vertices = assembleVertices(positions, normals, texUVs);
+	std::vector<GLuint> indices = getIndices(JSON["accessors"][indAccInd]);
+	std::vector<Texture> textures = getTextures();
+
+	// Combine the vertices, indices, and textures into a mesh
+	meshes.push_back(Mesh(vertices, indices, textures));
+}
+
+
+void Model::traverseNode(unsigned int nextNode, glm::mat4 matrix)
+{
+	// Current node
+	json node = JSON["nodes"][nextNode];
+
+	// Get translation if it exists
+	glm::vec3 translation = glm::vec3(0.0f, 0.0f, 0.0f);
+	if (node.find("translation") != node.end())
+	{
+		float transValues[3];
+		for (unsigned int i = 0; i < node["translation"].size(); i++)
+			transValues[i] = (node["translation"][i]);
+		translation = glm::make_vec3(transValues);
+	}
+	// Get quaternion if it exists
+	glm::quat rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+	if (node.find("rotation") != node.end())
+	{
+		float rotValues[4] =
+		{
+			node["rotation"][3],
+			node["rotation"][0],
+			node["rotation"][1],
+			node["rotation"][2]
+		};
+		rotation = glm::make_quat(rotValues);
+	}
+	// Get scale if it exists
+	glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+	if (node.find("scale") != node.end())
+	{
+		float scaleValues[3];
+		for (unsigned int i = 0; i < node["scale"].size(); i++)
+			scaleValues[i] = (node["scale"][i]);
+		scale = glm::make_vec3(scaleValues);
+	}
+	// Get matrix if it exists
+	glm::mat4 matNode = glm::mat4(1.0f);
+	if (node.find("matrix") != node.end())
+	{
+		float matValues[16];
+		for (unsigned int i = 0; i < node["matrix"].size(); i++)
+			matValues[i] = (node["matrix"][i]);
+		matNode = glm::make_mat4(matValues);
+	}
+
+	// Initialize matrices
+	glm::mat4 trans = glm::mat4(1.0f);
+	glm::mat4 rot = glm::mat4(1.0f);
+	glm::mat4 sca = glm::mat4(1.0f);
+
+	// Use translation, rotation, and scale to change the initialized matrices
+	trans = glm::translate(trans, translation);
+	rot = glm::mat4_cast(rotation);
+	sca = glm::scale(sca, scale);
+
+	// Multiply all matrices together
+	glm::mat4 matNextNode = matrix * matNode * trans * rot * sca;
+
+	// Check if the node contains a mesh and if it does load it
+	if (node.find("mesh") != node.end())
+	{
+		translationsMeshes.push_back(translation);
+		rotationsMeshes.push_back(rotation);
+		scalesMeshes.push_back(scale);
+		matricesMeshes.push_back(matNextNode);
+
+		loadMesh(node["mesh"]);
+	}
+
+	// Check if the node has children, and if it does, apply this function to them with the matNextNode
+	if (node.find("children") != node.end())
+	{
+		for (unsigned int i = 0; i < node["children"].size(); i++)
+			traverseNode(node["children"][i], matNextNode);
+	}
+}
+
+std::vector<unsigned char> Model::getData()
+{
+	// Create a place to store the raw text, and get the uri of the .bin file
+	std::string bytesText;
+	std::string uri = JSON["buffers"][0]["uri"];
+
+	// Store raw text data into bytesText
+	std::string fileStr = std::string(file);
+	std::string fileDirectory = fileStr.substr(0, fileStr.find_last_of('/') + 1);
+	bytesText = get_file_contents((fileDirectory + uri).c_str());
+
+	// Transform the raw text data into bytes and put them in a vector
+	std::vector<unsigned char> data(bytesText.begin(), bytesText.end());
+	return data;
+}
+
+std::vector<float> Model::getFloats(json accessor)
+{
+	std::vector<float> floatVec;
+
+	// Get properties from the accessor
+	unsigned int buffViewInd = accessor.value("bufferView", 1);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	std::string type = accessor["type"];
+
+	// Get properties from the bufferView
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	// Interpret the type and store it into numPerVert
+	unsigned int numPerVert;
+	if (type == "SCALAR") numPerVert = 1;
+	else if (type == "VEC2") numPerVert = 2;
+	else if (type == "VEC3") numPerVert = 3;
+	else if (type == "VEC4") numPerVert = 4;
+	else throw std::invalid_argument("Type is invalid (not SCALAR, VEC2, VEC3, or VEC4)");
+
+	// Go over all the bytes in the data at the correct place using the properties from above
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	unsigned int lengthOfData = count * 4 * numPerVert;
+	for (unsigned int i = beginningOfData; i < beginningOfData + lengthOfData; i)
+	{
+		unsigned char bytes[] = { data[i++], data[i++], data[i++], data[i++] };
+		float value;
+		std::memcpy(&value, bytes, sizeof(float));
+		floatVec.push_back(value);
+	}
+
+	return floatVec;
+}
+
+std::vector<GLuint> Model::getIndices(json accessor)
+{
+	std::vector<GLuint> indices;
+
+	// Get properties from the accessor
+	unsigned int buffViewInd = accessor.value("bufferView", 0);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	unsigned int componentType = accessor["componentType"];
+
+	// Get properties from the bufferView
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	// Get indices with regards to their type: unsigned int, unsigned short, or short
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	if (componentType == 5125)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 4; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++], data[i++], data[i++] };
+			unsigned int value;
+			std::memcpy(&value, bytes, sizeof(unsigned int));
+			indices.push_back((GLuint)value);
+		}
+	}
+	else if (componentType == 5123)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++] };
+			unsigned short value;
+			std::memcpy(&value, bytes, sizeof(unsigned short));
+			indices.push_back((GLuint)value);
+		}
+	}
+	else if (componentType == 5122)
+	{
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i)
+		{
+			unsigned char bytes[] = { data[i++], data[i++] };
+			short value;
+			std::memcpy(&value, bytes, sizeof(short));
+			indices.push_back((GLuint)value);
+		}
+	}
+
+	return indices;
+}
+
+std::vector<Texture> Model::getTextures()
+{
+	std::vector<Texture> textures;
+
+	std::string fileStr = std::string(file);
+	std::string fileDirectory = fileStr.substr(0, fileStr.find_last_of('/') + 1);
+
+	// Go over all images
+	for (unsigned int i = 0; i < JSON["images"].size(); i++)
+	{
+		// uri of current texture
+		std::string texPath = JSON["images"][i]["uri"];
+
+		// Check if the texture has already been loaded
+		bool skip = false;
+		for (unsigned int j = 0; j < loadedTexName.size(); j++)
+		{
+			if (loadedTexName[j] == texPath)
+			{
+				textures.push_back(loadedTex[j]);
+				skip = true;
+				break;
+			}
+		}
+
+		// If the texture has been loaded, skip this
+		if (!skip)
+		{
+			// Load diffuse texture
+			if (texPath.find("baseColor") != std::string::npos)
+			{
+				Texture diffuse = Texture((fileDirectory + texPath).c_str(), "diffuse", loadedTex.size());
+				textures.push_back(diffuse);
+				loadedTex.push_back(diffuse);
+				loadedTexName.push_back(texPath);
+			}
+			// Load specular texture
+			else if (texPath.find("metallicRoughness") != std::string::npos)
+			{
+				Texture specular = Texture((fileDirectory + texPath).c_str(), "specular", loadedTex.size());
+				textures.push_back(specular);
+				loadedTex.push_back(specular);
+				loadedTexName.push_back(texPath);
+			}
+		}
+	}
+
+	return textures;
+}
+
+std::vector<Vertex> Model::assembleVertices
+(
+	std::vector<glm::vec3> positions,
+	std::vector<glm::vec3> normals,
+	std::vector<glm::vec2> texUVs
+)
+{
+	std::vector<Vertex> vertices;
+	for (int i = 0; i < positions.size(); i++)
+	{
+		vertices.push_back
+		(
+			Vertex
+			{
+				positions[i],
+				normals[i],
+				glm::vec3(1.0f, 1.0f, 1.0f),
+				texUVs[i]
+			}
+		);
+	}
+	return vertices;
+}
+
+std::vector<glm::vec2> Model::groupFloatsVec2(std::vector<float> floatVec)
+{
+	std::vector<glm::vec2> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec2(floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
+std::vector<glm::vec3> Model::groupFloatsVec3(std::vector<float> floatVec)
+{
+	std::vector<glm::vec3> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec3(floatVec[i++], floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
+std::vector<glm::vec4> Model::groupFloatsVec4(std::vector<float> floatVec)
+{
+	std::vector<glm::vec4> vectors;
+	for (int i = 0; i < floatVec.size(); i)
+	{
+		vectors.push_back(glm::vec4(floatVec[i++], floatVec[i++], floatVec[i++], floatVec[i++]));
+	}
+	return vectors;
+}
+
+	//Loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
+	void Model::loadObj(std::string const& path)
+	{
+		//Read file via ASSIMP
+		Assimp::Importer importer;
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals
+			| aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+		//Check for errors
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
+		{
+			std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+			return;
+		}
+		//Retrieve the directory path of the filepath
+		directory = path.substr(0, path.find_last_of('/'));
+
+		//Process ASSIMP's root node recursively
+		processNode(scene->mRootNode, scene);
+	}
+
+	//Processes a node in a recursive fashion
+	void Model::processNode(aiNode* node, const aiScene* scene)
+	{
+		//process each mesh located at the current node
+		for (unsigned int i = 0; i < node->mNumMeshes; i++)
+		{
+			//the node object only contains indices to index the actual objects in the scene. 
+			//the scene contains all the data, node is just to keep stuff organized (like relations between nodes).
+			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+			meshes.push_back(processMesh(mesh, scene));
+		}
+		//after we've processed all of the meshes (if any) we then recursively process each of the children nodes
+		for (unsigned int i = 0; i < node->mNumChildren; i++)
+		{
+			processNode(node->mChildren[i], scene);
+		}
+
+	}
+
+	void Model::Mesh processMesh(aiMesh* mesh, const aiScene* scene)
+	{
+		//data to fill
+		std::vector<Vertex> vertices;
+		std::vector<unsigned int> indices;
+		std::vector<Texture> textures;
+
+		//walk through each of the mesh's vertices
+		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+		{
+			Vertex vertex;
+			//we declare a placeholder vector since assimp uses its own vector class
+			//that doesn't directly convert to glm's vec3 class so
+			//we transfer the data to this placeholder glm::vec3 first
+			glm::vec3 vector;
+			//positions
+			vector.x = mesh->mVertices[i].x;
+			vector.y = mesh->mVertices[i].y;
+			vector.z = mesh->mVertices[i].z;
+			vertex.Position = vector;
+			//normals
+			if (mesh->HasNormals())
+			{
+				vector.x = mesh->mNormals[i].x;
+				vector.y = mesh->mNormals[i].y;
+				vector.z = mesh->mNormals[i].z;
+				vertex.Normal = vector;
+			}
+			//texture coordinates
+			if (mesh->mTextureCoords[0]) //does the mesh contain texture coordinates?
+			{
+				glm::vec2 vec;
+				//a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
+				//use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+				vec.x = mesh->mTextureCoords[0][i].x;
+				vec.y = mesh->mTextureCoords[0][i].y;
+				vertex.TexCoords = vec;
+				//tangent
+				vector.x = mesh->mTangents[i].x;
+				vector.y = mesh->mTangents[i].y;
+				vector.z = mesh->mTangents[i].z;
+				vertex.Tangent = vector;
+				//bitangent
+				vector.x = mesh->mBitangents[i].x;
+				vector.y = mesh->mBitangents[i].y;
+				vector.z = mesh->mBitangents[i].z;
+				vertex.Bitangent = vector;
+			}
+			else
+				vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+
+			vertices.push_back(vertex);
+		}
+		//walk through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+		{
+			aiFace face = mesh->mFaces[i];
+			//retrieve all indices of the face and store them in the indices vector
+			for (unsigned int j = 0; j < face.mNumIndices; j++)
+				indices.push_back(face.mIndices[j]);
+		}
+		//process materials
+		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+		//we assume a convention for sampler names in the shaders. Each diffuse texture should be named
+		//as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
+		//Same applies to other texture as the following list summarizes:
+		//diffuse: texture_diffuseN
+		//specular: texture_specularN
+		//normal: texture_normalN
+
+		//1. diffuse maps
+		std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+		textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+		//2. specular maps
+		std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+		//3. normal maps
+		std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
+		textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
+		//4. height maps
+		std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
+		textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
+
+		//return a mesh object created from the extracted mesh data
+		return Mesh(vertices, indices, textures);
+	}
+
+	//checks all material textures of a given type and loads the textures if they're not loaded yet.
+	//the required info is returned as a Texture struct.
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName)
+	{
+		std::vector<Texture> textures;
+		for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+		{
+			aiString str;
+			mat->GetTexture(type, i, &str);
+			//check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
+			bool skip = false;
+			for (unsigned int j = 0; j < textures_loaded.size(); j++)
+			{
+				if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+				{
+					textures.push_back(textures_loaded[j]);
+					skip = true; //a texture with the same filepath has already been loaded, continue to next one. (optimization)
+					break;
+				}
+			}
+			if (!skip)
+			{   //if texture hasn't been loaded already, load it
+				Texture texture;
+				texture.id = TextureFromFile(str.C_Str(), this->directory);
+				texture.type = typeName;
+				texture.path = str.C_Str();
+				textures.push_back(texture);
+				textures_loaded.push_back(texture);
+
+
+				//store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
+			}
+		}
+		return textures;
+	}
+};
+
+
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma)
+{
+	std::string filename = std::string(path);
+
+	filename = directory + '/' + filename;
+
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
---
+++ LoadObject3/libs.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+#include"Vertex.h"
+#include"Primitives.h"
+#include"Shader.h"
+#include"Texture.h"
+#include"Material.h"
+#include"Mesh.h"
+#include"Model.h"
+#include"Light.h"
